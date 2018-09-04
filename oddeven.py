8f640bf344fa566a260a1b3fb31fbe1d98b2eef5
nishamnisha <41040371+nishamnisha@users.noreply.github.com>
2018-09-04 14:00:15 +0530

    Add files via upload

---
+++ oddeven.py
@@ -0,0 +1,8 @@
+num = int(input("Enter any number: "))
+n = num%2
+if n == 0:
+    print(num, "is an even number")
+elif flag == 1:
+    print(num, "is an odd number")
+else:
+    print("Error, Invalid input")
